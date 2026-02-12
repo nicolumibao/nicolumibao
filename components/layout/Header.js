f995b258e195c6edb7b66349a95b29196a9d3f5a
nico-reakt <nicolumibao@gmail.com>
2026-02-12 10:40:43 +0800

    chore: update profile
    Signed-off-by: nico-reakt <nicolumibao@gmail.com>

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,22 +1,42 @@
-import sitedetails from '@/lib/constant/sitedetails';
-import Head from 'next/head';
+import sitedetails from "@/lib/constant/sitedetails";
+import Head from "next/head";
 
-export default function Header ({title = sitedetails.position}) {
+export default function Header({ title = sitedetails.position }) {
   return (
     <Head>
-
       <link rel="icon" href={sitedetails.favivon} />
-      <title>{sitedetails.fullname} - {title}</title>
+      <title>
+        {sitedetails.fullname} - {title}
+      </title>
       <meta name="description" content={sitedetails.about} />
       <meta name="author" content={sitedetails.fullname} />
-      <meta name="keywords" content={`${sitedetails.fullname}, ${sitedetails.position}`} />
+      <meta
+        name="keywords"
+        content={`${sitedetails.fullname}, ${sitedetails.position}`}
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta name="url" property="og:url" content={sitedetails.link} />
-      <meta name="site_name" property="og:site_name" content={sitedetails.fullname}/>
-      <meta name="description" property="og:description" content={sitedetails.about} />
-      <meta name="secure_url" property="og:image:secure_url" content="https://nicolumibao.vercel.app/images/nico.jpg" />
-      <meta name="image" property="og:image" content="https://nicolumibao.vercel.app/images/nico.jpg" />
+      <meta
+        name="site_name"
+        property="og:site_name"
+        content={sitedetails.fullname}
+      />
+      <meta
+        name="description"
+        property="og:description"
+        content={sitedetails.about}
+      />
+      <meta
+        name="secure_url"
+        property="og:image:secure_url"
+        content="https://nicolumibao.pages.dev/images/nico.jpg"
+      />
+      <meta
+        name="image"
+        property="og:image"
+        content="https://nicolumibao.pages.dev/images/nico.jpg"
+      />
       <meta name="type" property="og:image:type" content="jpg" />
 
       <meta name="twitter:card" content="summary_large_image" />
@@ -24,9 +44,11 @@ export default function Header ({title = sitedetails.position}) {
       <meta name="twitter:creator" content={sitedetails.fullname} />
       <meta name="twitter:title" content={sitedetails.fullname} />
       <meta name="twitter:description" content={sitedetails.about} />
-      <meta name="twitter:image" content="https://nicolumibao.vercel.app/images/nico.jpg" />
+      <meta
+        name="twitter:image"
+        content="https://nicolumibao.pages.dev/images/nico.jpg"
+      />
       <meta name="twitter:domain" content={sitedetails.link} />
-
     </Head>
-  )
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,46 @@
 import "@/styles/globals.css";
 import DefaultLayout from "@/components/layout/DefaultLayout";
-import Head from 'next/head';
-import sitedetails from '@/lib/constant/sitedetails';
+import Head from "next/head";
+import sitedetails from "@/lib/constant/sitedetails";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
         <title>Nico Lumibao - Software Developer</title>
-        <meta name="google-site-verification" content="1O2tTtaH35H5oFSuQd0DfpBw_lhRa6ONoDsH-GYbftY" />
+        <meta
+          name="google-site-verification"
+          content="1O2tTtaH35H5oFSuQd0DfpBw_lhRa6ONoDsH-GYbftY"
+        />
         <meta name="description" content={sitedetails.about} />
         <meta name="author" content={sitedetails.fullname} />
-        <meta name="keywords" content={`${sitedetails.fullname}, ${sitedetails.position}`} />
+        <meta
+          name="keywords"
+          content={`${sitedetails.fullname}, ${sitedetails.position}`}
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <meta name="url" property="og:url" content={sitedetails.link} />
-        <meta name="site_name" property="og:site_name" content={sitedetails.fullname}/>
-        <meta name="description" property="og:description" content={sitedetails.about} />
-        <meta name="secure_url" property="og:image:secure_url" content="https://nicolumibao.vercel.app/images/nico.jpg" />
-        <meta name="image" property="og:image" content="https://nicolumibao.vercel.app/images/nico.jpg" />
+        <meta
+          name="site_name"
+          property="og:site_name"
+          content={sitedetails.fullname}
+        />
+        <meta
+          name="description"
+          property="og:description"
+          content={sitedetails.about}
+        />
+        <meta
+          name="secure_url"
+          property="og:image:secure_url"
+          content="https://nicolumibao.pages.dev/images/nico.jpg"
+        />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://nicolumibao.pages.dev/images/nico.jpg"
+        />
         <meta name="type" property="og:image:type" content="jpg" />
 
         <meta name="twitter:card" content="summary_large_image" />
@@ -26,7 +48,10 @@ export default function App({ Component, pageProps }) {
         <meta name="twitter:creator" content={sitedetails.fullname} />
         <meta name="twitter:title" content={sitedetails.fullname} />
         <meta name="twitter:description" content={sitedetails.about} />
-        <meta name="twitter:image" content="https://nicolumibao.vercel.app/images/nico.jpg" />
+        <meta
+          name="twitter:image"
+          content="https://nicolumibao.pages.dev/images/nico.jpg"
+        />
         <meta name="twitter:domain" content={sitedetails.link} />
       </Head>
       <DefaultLayout>
